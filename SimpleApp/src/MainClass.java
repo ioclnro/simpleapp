2da466c2a73193c5524108425815091e23d76926
Gufran Khurshid <guffy1267@gmail.com>
2014-08-23 15:45:38 +0530

    Worked on Third Line

--- SimpleApp/src/MainClass.java
+++ SimpleApp/src/MainClass.java
@@ -4,6 +4,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome  Java");
 		System.out.println("Second Line");
+		System.out.println("Third Line");
+		
 	}
 
 }
